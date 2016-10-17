7b36647a61f84d3a90936789437dcd17228dd7b7
Kieran Hogg <kieran.hogg@gmail.com>
2016-10-17 15:49:39 +0400

    fix conflicts

--- isams_tools.py
+++ isams_tools.py
@@ -1,9 +1,18 @@
 from ad_sync import ad_sync
 from register_reminder import register_reminder
 from settings import DEBUG
+
+import argparse
 import logging
 import os
 import sys
+
+from isams_tools.sync import sync
+from isams_tools.sync import new_sync
+from isams_tools.register_reminder import register_reminder as rr
+from settings import DEBUG
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # setup logging to file
 logger = logging.getLogger('root')
@@ -26,21 +35,6 @@ if not DEBUG:
     # Install exception handler
     sys.excepthook = my_handler
 
-# make sure this isn't called directly
-if __name__ == "__main__":
-    logger.critical('Please use bin/isams_tools instead')
-    sys.exit(1)
-
-# check we've got a settings file
-try:
-    from settings import *
-except ImportError:
-    logger.critical('You have not renamed settings_example.py to settings.py\n')
-    sys.exit(1)
-
-logger.info('Started isams_tools with arguments:' + str(sys.argv))
-
-
 def dispatch(module, **kwargs):
     """Run the correct module
 
@@ -52,8 +46,42 @@ def dispatch(module, **kwargs):
         if 'stage' not in kwargs:
             register_reminder.run()
         else:
-            register_reminder.run(kwargs['stage'])
+            rr.run(kwargs['stage'])
     elif module == 'ad_sync':
-        ad_sync.ADSync()
+        sync.SyncConnector()
+    elif module == 'new_sync':
+        new_sync.main()
     else:
         logger.critical("Incorrect module given")
+
+def main():
+    # check we've got a settings file
+    try:
+        from settings import DEBUG
+    except ImportError:
+        logger.critical('You have not renamed settings_example.py to settings.py')
+        sys.exit(1)
+
+    logger.info('Started isams_tools with arguments:' + str(sys.argv))
+
+    p = argparse.ArgumentParser(description='...')
+    p.add_argument('module', help='Name of the module to run')
+    p.add_argument('--args', required=False, nargs=1, metavar=('arg'), help='Optional arguments for the module')
+
+    args = p.parse_args()
+
+    if args.module == "register_reminder":
+        if args.args and int(args.args[0]) in range(1, 4):
+            dispatch('register_reminder', stage=int(args.args[0]))
+        else:
+            exit("register_reminder needs an argument of 1-3 for the stage, e.g. isams_tools register_reminder --args 1")
+    elif args.module == 'ad_sync':
+        dispatch('ad_sync')
+    elif args.module == 'new_sync':
+        dispatch('new_sync')
+    else:
+        exit("Incorrect module given")
+
+    return
+
+main()
